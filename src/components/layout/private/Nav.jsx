d89423cf2125ac7a7544f1e693ccbe6eb343c381
franciscoalfar@gmail.com <*Fran2811*>
2023-12-17 01:33:45 -0300

    correciones en estilos

--- src/components/layout/private/Nav.jsx
+++ src/components/layout/private/Nav.jsx
@@ -2,32 +2,22 @@ import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { Sidebar } from './Sidebar'
 
-import '../../../assets/css/barranav.css'
+import '../../../assets/css/customnav.css';
+
+
 
 export const Nav = () => {
-
-  const [isOpen, setIsOpen] = useState(true);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
 
 
 
   return (
     <>
-      <nav className="navbar">
-        <div className="menu-toggle" onClick={toggleMenu}>
-          <span className="bar">Inicio</span>
-          <span className="bar">Servicios</span>
-          <span className="bar">Acerca de</span>
-        </div>
-        <ul className={`nav-list ${isOpen ? 'menu-open' : ''}`}>
-          <li><a href="#">Inicio</a></li>
-          <li><a href="#">Servicios</a></li>
-          <li><a href="#">Acerca de</a></li>
-          <li><a href="#">Contacto</a></li>
-        </ul>
+      <nav className="nav flex-column custom-nav">
+        <NavLink className="nav-link active" aria-current="page" to="/auth/dashboard">Dashboard</NavLink>
+        <NavLink className="nav-link" to="/auth/consumo">Tabla de Gastos</NavLink>
+        <NavLink className="nav-link" to="/auth/gastos">Ingresar Gastos</NavLink>
+        <NavLink className="nav-link" to="/auth/perfil">Perfil</NavLink>
+        <NavLink className="nav-link" to="/auth/logout">Cerrar Sesión</NavLink>
       </nav>
 
     </>
