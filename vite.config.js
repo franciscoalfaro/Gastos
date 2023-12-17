139550d12708540a1ed77456f5b976aebc395947
franciscoalfar@gmail.com <*Fran2811*>
2023-12-17 02:15:52 -0300

    mejoras

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        // Opciones específicas de Rollup...
+        manualChunks(id) {
+          // Ejemplo de configuración de manualChunks
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Agrupa todos los módulos de node_modules en un chunk llamado 'vendor'
+          }
+        },
+      },
+    },
+  },
 })
