5ffbd091ed45ffc5d72d95b27fcf5bd9288c906e
franciscoalfar@gmail.com <*Fran2811*>
2023-12-26 16:03:47 -0300

    se corrige url

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,4 +1,4 @@
 export const Global={
-    url:"http://localhost:3000/api/"
-    //url:"https://api.comogasto.com/api/"
+    //url:"http://localhost:3000/api/"
+    url:"https://api.comogasto.com/api/"
 }
